d8d63a9b4f939cc8d8e3933e5c7b12d3d29c9f74
JonasBM <jonas.mela@gmail.com>
2022-03-01 20:04:13 +0100

    FEAT: add properties

--- src/main/java/com/example/moviecharactersapi/model/dbo/Franchise.java
+++ src/main/java/com/example/moviecharactersapi/model/dbo/Franchise.java
@@ -1,4 +1,27 @@
 package com.example.moviecharactersapi.model.dbo;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Franchise {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(nullable = false, length = 40)
+    private String Name;
+
+    @Column(length = 80)
+    private String description;
 }
