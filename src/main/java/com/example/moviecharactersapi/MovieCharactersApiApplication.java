759cd0c082f5fc44f6249fd66e1053ee6a039b52
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-01 16:08:04 +0100

    FEAT: now seeds data on application start

--- src/main/java/com/example/moviecharactersapi/MovieCharactersApiApplication.java
+++ src/main/java/com/example/moviecharactersapi/MovieCharactersApiApplication.java
@@ -1,5 +1,6 @@
 package com.example.moviecharactersapi;
 
+import com.example.moviecharactersapi.service.SeederService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,6 +8,12 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 @EnableWebMvc
 public class MovieCharactersApiApplication {
+
+    public MovieCharactersApiApplication(SeederService seederService) {
+        seederService.seedFranchises();
+        seederService.seedMovies();
+        seederService.seedCharacters();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MovieCharactersApiApplication.class, args);
