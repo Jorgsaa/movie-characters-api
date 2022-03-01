9ad25d15c4f2023c6d8132230e939306b309c6e7
JonasBM <jonas.mela@gmail.com>
2022-03-01 20:14:01 +0100

    CHORE: indicate that this is a repository

--- src/main/java/com/example/moviecharactersapi/repository/FranchiseRepository.java
+++ src/main/java/com/example/moviecharactersapi/repository/FranchiseRepository.java
@@ -2,5 +2,7 @@ package com.example.moviecharactersapi.repository;
 
 import com.example.moviecharactersapi.model.dbo.Franchise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FranchiseRepository extends JpaRepository<Franchise, Integer> {}
