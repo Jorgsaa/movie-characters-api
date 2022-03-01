32bc38e1463ab8f48ce2ed09c6ecefafe7a56372
JonasBM <jonas.mela@gmail.com>
2022-03-01 20:16:18 +0100

    FEAT: add franchise repository

--- src/main/java/com/example/moviecharactersapi/controller/FranchiseController.java
+++ src/main/java/com/example/moviecharactersapi/controller/FranchiseController.java
@@ -1,4 +1,15 @@
 package com.example.moviecharactersapi.controller;
 
+import com.example.moviecharactersapi.repository.FranchiseRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/franchise")
 public class FranchiseController {
+    private final FranchiseRepository franchises;
+
+    public FranchiseController(FranchiseRepository franchises) {
+        this.franchises = franchises;
+    }
 }
